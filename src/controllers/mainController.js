599ffb5ddd802e7c8afce0933d288d2708370fc4
Lauracs13 <lauracs1213@gmail.com>
2022-03-20 15:34:52 +0300

    imágenes organizadas y archivo JSON

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,11 +1,15 @@
-const path =require('path')
+const fs = require('fs');
+const path =require('path');
+// para configurar la base de datos provisional:
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const main={
     //se ocupa el método render para trabajar con view engine EJS
     index:(req,res)=>{
         res.render('home')
     },
     categories:(req,res)=>{
-        res.render('./productViews/categories')
+        res.render('./productViews/categories', {products})
     },
     detail:(req,res)=>{
         res.render('./productViews/detalle')
