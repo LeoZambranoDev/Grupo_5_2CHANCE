f4efd16eb0eaa23d392749ecf9afa24c8fedfd00
Zambrano-Leonardo <leonardo_zambrano@outlook.com>
2022-04-08 13:48:50 -0500

    imported middlewares of registerUser post

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -6,10 +6,21 @@ let router = express.Router()
 //controlador del objeto literal user(userController.js)
 const userController=require('../controllers/userController')
 
+// MULTER MIDDLEWARE
+const upload=require('../middleware/users/multerUser')
+// MULTER MIDDLEWARE
 
+// Middleware para encriptar la contraseña
+const bcrypt =require('../middleware/users/encPassUser')
+// Middleware para encriptar la contraseña
+
+// Middleware para validar los campos provenientes del form
+const validationUserRegister =require('../middleware/users/validationUserRegister')
+// Middleware para validar los campos provenientes del form
 //RUTAS
 router.get('/login',userController.login)
-router.get('/register',userController.register)
+router.get('/register',userController.registerView)
+router.post('/register',upload.single('img-perfil'),validationUserRegister,bcrypt.encPass,userController.register)
 
 //IMPORTANTE: exportar el módulo para poder ser usado en app.js
 module.exports=router
