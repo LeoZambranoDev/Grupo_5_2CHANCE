4cbe860ee4f69e6c47652cc21bc99cbc0056899a
Zambrano-Leonardo <leonardo_zambrano@outlook.com>
2022-04-08 13:45:10 -0500

    Creada la función para guardar un usuario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,9 +4,66 @@ const user={
     login:(req,res)=>{
         res.render('./userViews/login')
     },
-    register:(req,res)=>{
+    registerView:(req,res)=>{
         res.render('./userViews/registerUser')
     },
+    register:(req,res)=>{
+        //Requerimos la función para capturar los errores almacenados en req
+        const {validationResult}=require('express-validator')
+        let errors=validationResult(req)
+        let errorsList=errors.errors
+        //Requerimos todos los usuarios almacenados para agregar el nuevo y escribirlos en en json
+        const userModel=require('../models/userModel')
+        let usersList=userModel.getUsers()
+
+        //Verificamos incialmente la validación de los inputs del form
+        if (errors.isEmpty()) {
+            
+            //Vereificamos que el proceso de comparar y encriptar las contras haya salido bien(true)
+            if(req.body.passConfirm){
+                
+                // Verificamos que haya una imagen de perfil y asignamos en una variable para su posrterior uso
+                let nameImg='default.jpg'
+                if(req.file){
+                    nameImg=req.file.filename
+                }
+                //creamos el usuario para ser guardado
+                let user={
+                    "id": usersList.length + 1,
+                    "firstName": "",
+                    "lastName": "",
+                    "nick":req.body.nick,
+                    "email" : req.body.email,
+                    "pass": req.body.pass,
+                    "address": "",
+                    "type": "client",
+                    "image": nameImg,
+                    "status":"active"
+                }
+                //agregamos el usuario a la lista
+                usersList.push(user)
+
+                //Guardamos en el json
+                userModel.saveUserList(usersList)
+                res.redirect("/")
+            }else{
+                //agregamos un error para cuando las contraseñas no coinciden
+                errorsList.push({
+                    value: '',
+                    msg: 'Las contraseñas deben coincidir',
+                    param: 'passConfirm',
+                    location: 'body'
+                  })
+
+                  //enviamos el error a la vista
+                  res.render('userViews/registerUser',{errorsList})
+            }
+        }
+        else{
+            console.log(errorsList);
+            res.render('userViews/registerUser',{errorsList})
+        }
+    }
 }
 
 
