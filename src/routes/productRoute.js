42ac1d77563112ac4e53303fb1c5c84fff846ae6
Zambrano-Leonardo <leonardo_zambrano@outlook.com>
2022-04-14 23:55:09 -0500

    Reubicación de multer

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -2,32 +2,20 @@
 let express =require('express')
 let router = express.Router()
 const path= require('path')
+
 // CONFIGURACIÓN MULTER
-const multer = require('multer')
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.resolve(__dirname, '../../public/img/products'))
-    },
-    filename: function (req, file, cb) {
-    
-        let newName= Date.now() + file.originalname
-      cb(null, newName)
-    }
-  })
-
-const upload = multer({ storage })
-
-
+const uploadProduct = require('../middleware/products/multerProduct')
 
 //controlador del objeto literal product(productController.js)
 const productController=require('../controllers/productController')
 
-//RUTAS
+//Rutas para vistas(ejs)
 router.get('/register',productController.register) // view register
 router.get('/update/:id',productController.update)     //view update
-router.post('/',upload.single('imgCel'),productController.store)    // add product
 
-router.put('/:id',upload.single('imgCel'),productController.updateProduct)      //update product
+//Rutas por métodos != get
+router.post('/',uploadProduct.single('imgCel'),productController.store)    // add product
+router.put('/:id',uploadProduct.single('imgCel'),productController.updateProduct)//update product
 router.delete('/:id',productController.deleteProduct)      //delete product
 
 //IMPORTANTE: exportar el módulo para poder ser usado en app.js
