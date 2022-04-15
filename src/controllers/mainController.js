e07f59c3a3072248dcdd8c9416b1c462d7864360
Zambrano-Leonardo <leonardo_zambrano@outlook.com>
2022-04-15 00:22:01 -0500

    Se movieron funciones a productcontroller

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -9,18 +9,6 @@ const main={
         let products=productModel.getProductByCategory('destacado')
         let productPro=productModel.getProductById(8)// producto temporal
         res.render('home',{products,productPro})
-    },
-    categories:(req,res)=>{
-        let products=productModel.getProducts()
-        res.render('./productViews/categories', {products})
-    },
-    detail:(req,res)=>{
-        
-        let products=productModel.getProductByCategory('destacado')
-        res.render('./productViews/detalle',{products})
-    },
-    shopingCart:(req,res)=>{
-        res.render('./productViews/shopCart')
     }
 }
 
