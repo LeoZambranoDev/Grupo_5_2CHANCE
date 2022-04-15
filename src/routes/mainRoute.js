4c74fc7fb7583b789628b699fd05cabaae771307
Zambrano-Leonardo <leonardo_zambrano@outlook.com>
2022-04-15 00:22:54 -0500

    Se movieron las rutas

--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -2,16 +2,11 @@
 let express =require('express')
 let router = express.Router()
 
-
 //controlador del objeto literal main(mainController.js)
 const mainController=require('../controllers/mainController')
 
-
 //RUTAS
 router.get('/',mainController.index)
-router.get('/categories',mainController.categories)
-router.get('/detail',mainController.detail)
-router.get('/shopingCart', mainController.shopingCart)
 
 //IMPORTANTE: exportar el módulo para poder ser usado en app.js
 module.exports=router
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -10,13 +10,19 @@ const uploadProduct = require('../middleware/products/multerProduct')
 const productController=require('../controllers/productController')
 
 //Rutas para vistas(ejs)
-router.get('/register',productController.register) // view register
-router.get('/update/:id',productController.update)     //view update
+router.get('/register',productController.register)              //view register
+router.get('/update/:id',productController.update)              //view update
+router.get('/categories',productController.categoriesView)      //view Categories
+router.get('/detail',productController.detailView)              //view DetailProduct
+router.get('/shopingCart', productController.shopingCartView)   //view ShoppingCart
 
 //Rutas por métodos != get
 router.post('/',uploadProduct.single('imgCel'),productController.store)    // add product
 router.put('/:id',uploadProduct.single('imgCel'),productController.updateProduct)//update product
 router.delete('/:id',productController.deleteProduct)      //delete product
+
+
+
 
 //IMPORTANTE: exportar el módulo para poder ser usado en app.js
 module.exports=router
