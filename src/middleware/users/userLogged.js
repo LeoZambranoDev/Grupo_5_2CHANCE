f802a9054206772bcdc9033f1c116ae6af88472b
Zambrano-Leonardo <leonardo_zambrano@outlook.com>
2022-04-26 21:05:18 -0500

    New middleware

---
+++ src/middleware/users/userLogged.js
@@ -0,0 +1,21 @@
+const User = require('../../models/userModel');
+
+function userLoggedMiddleware(req, res, next) {
+	res.locals.isLogged = false;
+
+	let emailInCookie = req.cookies.email;
+	let userFromCookie = User.getUserByField('email', emailInCookie);
+
+	if (userFromCookie) {
+		req.session.userLogged = userFromCookie;
+	}
+
+	if (req.session.userLogged) {
+		res.locals.isLogged = true;
+		res.locals.userLogged = req.session.userLogged;
+	}
+
+	next();
+}
+
+module.exports = userLoggedMiddleware;
