e778f323904d731673acaa4ea19c4a5b5ad42b6f
Zambrano-Leonardo <leonardo_zambrano@outlook.com>
2022-05-17 22:17:31 -0500

    adaptado para sequelize

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 const path =require('path');
+const db = require('../data/models');
 
 const productModel=require('../models/productModel')
 
 const main={
     //se ocupa el método render para trabajar con view engine EJS
-    index:(req,res)=>{
-        let products=productModel.getProductByCategory('destacado')
-        let productPro=productModel.getProductById(8)// producto temporal
+    index: async(req,res)=>{
+        //Obenemos el listado de productos que pertenecen a la categoría Main
+        let productsMain= await productModel.getProductByCategory('Main')
+        //Temporalmente solo enviamos un product 
+        let productPro=productsMain[0]
+        //Obenemos el listado de productos
+        let products= await productModel.getProducts()
         res.render('home',{products,productPro})
     }
 }
