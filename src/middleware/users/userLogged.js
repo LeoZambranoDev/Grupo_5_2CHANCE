0ed840f0e5d9643ee8c7f71df354af3c6c43279e
Zambrano-Leonardo <leonardo_zambrano@outlook.com>
2022-05-17 22:36:25 -0500

    Cambios míimos

--- src/middleware/users/userLogged.js
+++ src/middleware/users/userLogged.js
@@ -1,15 +1,21 @@
 const User = require('../../models/userModel');
 
-function userLoggedMiddleware(req, res, next) {
+async function userLoggedMiddleware(req, res, next) {
+	//Iniciamos en false la variable
 	res.locals.isLogged = false;
 
-	let emailInCookie = req.cookies.email;
-	let userFromCookie = User.getUserByField('email', emailInCookie);
-
+	let emailInCookie=''
+	let userFromCookie
+	//verificamos si hay un email en cookie y lo obtenemos si hay
+	if (req.cookies.email) {
+		emailInCookie = req.cookies.email;
+		userFromCookie = await  User.getUserByEmail( emailInCookie);
+	}
+	//Verificamos si hay un usuario que corresponda al email
 	if (userFromCookie) {
 		req.session.userLogged = userFromCookie;
 	}
-
+	//verificamos si fue almacenado
 	if (req.session.userLogged) {
 		res.locals.isLogged = true;
 		res.locals.userLogged = req.session.userLogged;
