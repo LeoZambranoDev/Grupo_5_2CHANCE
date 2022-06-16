07031152e47748103a3c3cfee0aa90db2b0a69dc
Lauracs13 <lauracs1213@gmail.com>
2022-06-16 18:16:52 -0500

    datos de productos actualizados

--- SqlsDB/sqls/database.sql
+++ SqlsDB/sqls/database.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `bd_project_sprints` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `bd_project_sprints`;
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: bd_project_sprints
 -- ------------------------------------------------------
--- Server version	8.0.23
+-- Server version	8.0.29
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,8 +29,18 @@ CREATE TABLE `brands` (
   `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `brands`
+--
+
+LOCK TABLES `brands` WRITE;
+/*!40000 ALTER TABLE `brands` DISABLE KEYS */;
+INSERT INTO `brands` VALUES (1,'Samsung'),(2,'Apple'),(3,'Xiaomi'),(4,'Vivo'),(5,'Oppo'),(6,'Huawei'),(7,'Motorola'),(8,'LG'),(9,'Sony'),(10,'Nokia'),(11,'Poco'),(12,'Realme');
+/*!40000 ALTER TABLE `brands` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `carts`
@@ -52,6 +62,15 @@ CREATE TABLE `carts` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `carts`
+--
+
+LOCK TABLES `carts` WRITE;
+/*!40000 ALTER TABLE `carts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `carts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `categories`
 --
 
@@ -67,6 +86,16 @@ CREATE TABLE `categories` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Main'),(2,'MostViewed'),(3,'InSale');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `colors`
 --
 
@@ -80,6 +109,16 @@ CREATE TABLE `colors` (
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `colors`
+--
+
+LOCK TABLES `colors` WRITE;
+/*!40000 ALTER TABLE `colors` DISABLE KEYS */;
+INSERT INTO `colors` VALUES (1,'Azul'),(2,'Rojo'),(3,'Negro'),(4,'Amarillo'),(5,'Blanco'),(6,'Gris'),(7,'Rosa'),(8,'Dorado'),(9,'Otro');
+/*!40000 ALTER TABLE `colors` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `details`
@@ -102,6 +141,15 @@ CREATE TABLE `details` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `details`
+--
+
+LOCK TABLES `details` WRITE;
+/*!40000 ALTER TABLE `details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `memories`
 --
 
@@ -115,6 +163,16 @@ CREATE TABLE `memories` (
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `memories`
+--
+
+LOCK TABLES `memories` WRITE;
+/*!40000 ALTER TABLE `memories` DISABLE KEYS */;
+INSERT INTO `memories` VALUES (1,'4GB'),(2,'8GB'),(3,'16GB'),(4,'32GB'),(5,'64GB'),(6,'128GB'),(7,'256GB');
+/*!40000 ALTER TABLE `memories` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `products`
@@ -152,6 +210,16 @@ CREATE TABLE `products` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'iPhone 8 Plus',1500000,NULL,'iPhone8Plus.jpg',3,2,5,5,NULL,NULL,NULL),(2,'iPhone 12 mini',2700000,NULL,'iph12minihorizontal.jpg',6,2,6,6,NULL,NULL,NULL),(3,'iPhone 12 pro max',5000000,2,'12promaxhorizontal.jpg',6,2,7,7,NULL,NULL,NULL),(4,'Samsung Galaxy Note 10 lite',1300000,2,'GalaxyNote10Lite.jpg',3,1,6,6,NULL,NULL,NULL),(5,'Huawei Y7',350000,2,'HuaweiY7.jpg',8,6,5,5,NULL,NULL,NULL),(6,'iPhone 13 ',3100000,1,'ip.jpg',3,2,7,7,NULL,NULL,NULL),(7,'iPhone 12 pro',3800000,2,'ip12pro.jpg',6,2,6,6,NULL,NULL,NULL),(8,'LG k10 ',375000,2,'LGK10Lte.jpg',3,8,4,4,NULL,NULL,NULL),(9,'Motorola Nexus 6',700000,NULL,'MotorolaNexus6.jpg',5,7,4,4,NULL,NULL,NULL),(10,'Nokia 1100 ',20000000,NULL,'nokia.jpg',6,10,1,1,NULL,NULL,NULL),(11,'Oppo A54',900000,3,'OppoA54.jpg',1,5,6,6,NULL,NULL,NULL),(12,'Realme GT 5G',1100000,3,'RealmeGt5g.jpg',4,12,6,6,NULL,NULL,NULL),(13,'Sony Xperia Gen 1',2800000,3,'SonyXperia.jpg',3,9,2,2,NULL,NULL,NULL),(14,'Xiaomi 12',3000000,3,'45461231654654654.jpg',1,3,5,5,NULL,NULL,NULL),(15,'Poco F4 GT',1850000,3,'5646542313456.jpg',4,11,6,6,NULL,NULL,NULL),(16,'Xiaomi Redmi 10',790000,NULL,'8875123185465465.jpg',1,3,6,6,NULL,NULL,NULL),(17,'LG Flex 2 ',500000,NULL,'33221545748.jpg',3,8,5,5,NULL,NULL,NULL),(18,'Sony Ericsson Xperia Neo V',290000,NULL,'456465456456231231321.jpg',3,9,4,4,NULL,NULL,NULL),(19,'Sony Ericsson W580',315000,NULL,'88979875423123156456.jpg',5,9,4,4,NULL,NULL,NULL),(20,'Nokia X2-00',190000,NULL,'54648974654321231.jpg',5,10,5,5,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `rams`
 --
 
@@ -167,6 +235,16 @@ CREATE TABLE `rams` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `rams`
+--
+
+LOCK TABLES `rams` WRITE;
+/*!40000 ALTER TABLE `rams` DISABLE KEYS */;
+INSERT INTO `rams` VALUES (1,'2GB'),(2,'4GB'),(3,'8GB'),(4,'16GB'),(5,'32GB'),(6,'64GB'),(7,'128GB');
+/*!40000 ALTER TABLE `rams` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `types`
 --
 
@@ -180,6 +258,16 @@ CREATE TABLE `types` (
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `types`
+--
+
+LOCK TABLES `types` WRITE;
+/*!40000 ALTER TABLE `types` DISABLE KEYS */;
+INSERT INTO `types` VALUES (1,'Admin'),(2,'Client'),(3,'Employee');
+/*!40000 ALTER TABLE `types` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -209,6 +297,16 @@ CREATE TABLE `users` (
   CONSTRAINT `fk_Users_types` FOREIGN KEY (`type_id`) REFERENCES `types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,NULL,NULL,'Lola','lola@correo.com','12345',NULL,'default.jpg',1,NULL,NULL,NULL),(2,NULL,NULL,'Ernesto Pérez','ernestino@correo.com','12345',NULL,'1650313517466.png',NULL,NULL,NULL,NULL),(4,'Pepito','Pérez','Pepito','pepito@gmail.com','12345','calle123','default.jpg',NULL,NULL,NULL,NULL),(5,'User One','ningunooooo','usuario1','usuario1@email.com','$2a$10$D4l56.k/u15M85G0aXkyquOWvdWOA/XZuTUFbmbpPs0D8Jk3jIc1K','calle falsa 1235555','default.png',1,'2022-05-17','2022-05-19',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -219,4 +317,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-24 19:44:14
+-- Dump completed on 2022-06-16 18:11:22
